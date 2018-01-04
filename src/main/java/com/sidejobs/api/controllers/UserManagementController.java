b96a1b3a2f2b75e5feec971f57d2513d623e0e82
D'Mita Levy <dlevy022@fiu.edu>
2018-01-04 18:33:00 -0500

    Modified registration and verification responses to add user data

--- src/main/java/com/sidejobs/api/controllers/UserManagementController.java
+++ src/main/java/com/sidejobs/api/controllers/UserManagementController.java
@@ -2,6 +2,7 @@ package com.sidejobs.api.controllers;
 
 import java.io.UnsupportedEncodingException;
 import java.sql.Timestamp;
+import java.util.Collection;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -28,6 +29,7 @@ import com.sidejobs.api.common.ResponseCodes;
 import com.sidejobs.api.common.ResponseStatus;
 import com.sidejobs.api.common.ResponseWrapper;
 import com.sidejobs.api.common.VerificationResponse;
+import com.sidejobs.api.entities.Specialty;
 import com.sidejobs.api.entities.User;
 import com.sidejobs.api.entities.Verification;
 import com.sidejobs.api.repositories.UsersRepository;
@@ -56,6 +58,21 @@ public class UserManagementController {
 	{
 		this.usersRepository = usersRepository;
 		this.verificationsRepository = verificationsRepository;
+	}
+	
+	@RequestMapping(value="/search/user/{id}", method=RequestMethod.GET)
+	public ResponseWrapper<User> getUser(@PathVariable("id") String id){
+		
+		ResponseWrapper<User> result = new ResponseWrapper<User>(ResponseStatus.Failure);
+		
+		User user = this.usersRepository.findUserById(id);
+		
+		if(user == null)
+			return result;
+		
+		result =  new ResponseWrapper<User>(ResponseStatus.Success,user);
+			
+		return result;
 	}
 	
 	@RequestMapping(value="/login", method=RequestMethod.POST)
@@ -100,89 +117,105 @@ public class UserManagementController {
 		return result;
 	}
 	
+	/***
+	 * Verifies a registration email token is valid and then closes the registration token and sets the user status to "Active"
+	 * @param token
+	 * @return
+	 */
 	@RequestMapping(value="/verification/{token}", method=RequestMethod.GET)
 	public ResponseWrapper<VerificationResponse> getTokenVerificationStatus(@PathVariable String token)
 	{
 		ResponseWrapper<VerificationResponse> result = new ResponseWrapper<VerificationResponse>(ResponseStatus.Failure);
 		Verification veri = this.verificationsRepository.findVerificationByToken(token);
 		
-		System.out.println("Verifying token");
+		logger.debug("Verifying email registration token: "+token);
 		if(veri == null)
 			return result;
 		
 		if(!veri.isTokenValid(new Timestamp(System.currentTimeMillis())))
 		{
-			System.out.println("Token is not valid");
+			logger.debug("Registration token: "+token+" is not valid based on timestamp verification checks");
 			return result;
 
 		}
 		
+		logger.debug("Closing registration token: "+token);
 		this.verificationsRepository.closeVerificationToken(token);
 		this.usersRepository.setUserAccountVerified(veri.getUserId());
+		this.usersRepository.changeUserAccountStatus(veri.getUserId(), "Active");
+		User user = this.usersRepository.findUserById(veri.getUserId());
 		
-		System.out.println("Token verified");
+		logger.debug("Registration token verified");
 		
-		result =  new ResponseWrapper<VerificationResponse>(ResponseStatus.Success,new VerificationResponse(ResponseCodes.USER_REGISTRATION_SUCCESSFUL));
+		result =  new ResponseWrapper<VerificationResponse>(ResponseStatus.Success,new VerificationResponse(ResponseCodes.USER_REGISTRATION_SUCCESSFUL,user));
 	
 		return result;
 	}
 	
+	/***
+	 * Reqister a new user with the role worker, the user will need to verify their account via a confirmation email. 
+	 * Until then the account status will remain as "Unverified"
+	 * 
+	 * @param request
+	 * @param email
+	 * @param firstName
+	 * @param lastName
+	 * @param password
+	 * @return
+	 * @throws MessagingException
+	 * @throws UnsupportedEncodingException
+	 */
 	@RequestMapping(value="/register/worker", method=RequestMethod.POST)
 	public ResponseWrapper<RegistrationResponse> registerStudent(
 			HttpServletRequest request,
-			@RequestParam("email")String email,
-			@RequestParam("firstname")String firstName,
-			@RequestParam("lastname")String lastName,
-			@RequestParam("password")String password
-
+			@RequestParam(value ="email", required=true)String email,
+			@RequestParam(value ="firstname", required=true)String firstName,
+			@RequestParam(value ="lastname", required=true)String lastName,
+			@RequestParam(value ="password", required=true)String password
 			) throws MessagingException, UnsupportedEncodingException{
+		
+		logger.debug("Recieved worker registration request with email: "+email+" firstname: "+firstName+" lastname: "+lastName+" password: "+password);
 		
 		ResponseWrapper<RegistrationResponse> response =  null;
 		
-		System.out.println("Checking if user exists");
+		logger.debug("Checking if user with email: "+email+" exists");
+		
 		//first check if the user already exists
 		Optional<User> existingUser = this.usersRepository.findUserByEmail(email);
 		
 		if(existingUser.isPresent())
-		{
-			return response = new ResponseWrapper<RegistrationResponse>(ResponseStatus.Failure,new RegistrationResponse(ResponseCodes.USER_EXISTS));
-		}
-		
+			return response = 
+				new ResponseWrapper<RegistrationResponse>(ResponseStatus.Failure,new RegistrationResponse(ResponseCodes.USER_EXISTS,null));
+			
 		//create a new password hash
-		System.out.println("Generating password hash...");
+		logger.debug("Generating password hash for new user with email: "+email);
 				
 		BCryptPasswordUtil passUtil = new BCryptPasswordUtil();
 		String passHash = passUtil.hash(password, 11);
 		
 		int userIdLength = Integer.parseInt(env.getProperty("sidejobs.user.id.length"));
 		String id = java.util.UUID.randomUUID().toString().replaceAll("-", "").substring(0,userIdLength);
-
-		System.out.println("id :"+id);
 				
 		if(this.usersRepository == null)
-			System.out.println("Users is null");
-		
-		System.out.println("id: "+id);
-		System.out.println("firstName: "+firstName);
-		System.out.println("lastName: "+lastName);
-		System.out.println("email: "+email);
-		System.out.println("passHash: "+passHash);
+			logger.info("Users Repository object is null after attempt to register worker with email: "+email);
 		
 		this.usersRepository.registerStudentUser(id, firstName, lastName, email, passHash, "");
 		
-		RegistrationResponse regResponse = new RegistrationResponse(ResponseCodes.USER_REGISTRATION_SUCCESSFUL);
-		regResponse.setUserId(id);
-		response = new ResponseWrapper<RegistrationResponse>(ResponseStatus.Success,regResponse);
-				
 		//send verification email
 		User u = this.usersRepository.findUserById(id);
 		
+		logger.debug("User registration successful for user with email: "+email);
+		
+		RegistrationResponse regResponse = new RegistrationResponse(ResponseCodes.USER_REGISTRATION_SUCCESSFUL,u);
+		response = new ResponseWrapper<RegistrationResponse>(ResponseStatus.Success,regResponse);
+						
 		String server = env.getProperty("sidejobs.web.token.path"); 
 		
 		MailClientUtil maiClient = new MailClientUtil(mailSender,u,server);
 		String token = maiClient.sendRegistrationConfirmation();
 		
-		verificationsRepository.insertVerificationToken(token, u.getId(), "New Registration");
+		logger.debug("Adding verification token to the verifications table");
+		this.verificationsRepository.insertVerificationToken(token, u.getId(), "New Registration");
 			
 		return response;
 
